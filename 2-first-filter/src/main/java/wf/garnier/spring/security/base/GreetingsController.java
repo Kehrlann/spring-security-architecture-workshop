3bc1e80936c632b927f12a80ac662626f68fe9d2
Daniel Garnier-Moiroux <git@garnier.wf>
2023-10-22 19:07:41 +0200

    first-filter: starting app

---
+++ 2-first-filter/src/main/java/wf/garnier/spring/security/base/GreetingsController.java
@@ -0,0 +1,29 @@
+package wf.garnier.spring.security.base;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+class GreetingsController {
+	@GetMapping("/")
+	public String publicPage() {
+		return "public";
+	}
+
+	@GetMapping("/private")
+	public String privatePage(Authentication authentication, Model model) {
+		model.addAttribute("name", getName(authentication));
+		return "private";
+	}
+
+	private static String getName(Authentication authentication) {
+		if (authentication.getPrincipal() instanceof OidcUser oidcUser) {
+			return oidcUser.getEmail();
+		}
+		return authentication.getName();
+	}
+
+}
