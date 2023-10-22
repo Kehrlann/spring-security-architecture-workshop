10c6dbbf26d1245727fc8fcc3b45f8e03a1911b2
Daniel Garnier-Moiroux <git@garnier.wf>
2023-10-22 18:31:59 +0200

    base-application: solution

--- 1-base-application/src/main/java/wf/garnier/spring/security/base/GreetingsController.java
+++ 1-base-application/src/main/java/wf/garnier/spring/security/base/GreetingsController.java
@@ -1,5 +1,7 @@
 package wf.garnier.spring.security.base;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,13 +14,16 @@ class GreetingsController {
 	}
 
 	@GetMapping("/private")
-	public String privatePage(Model model) {
-		model.addAttribute("name", getName());
+	public String privatePage(Authentication authentication, Model model) {
+		model.addAttribute("name", getName(authentication));
 		return "private";
 	}
 
-	private static String getName() {
-		return "UNKNOWN USER";
+	private static String getName(Authentication authentication) {
+		if (authentication.getPrincipal() instanceof OidcUser oidcUser) {
+			return oidcUser.getEmail();
+		}
+		return authentication.getName();
 	}
 
 }
