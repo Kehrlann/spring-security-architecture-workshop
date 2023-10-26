f8b541f22d642258ca87165886f044cdd5f50cc3
Daniel Garnier-Moiroux <git@garnier.wf>
2023-10-26 17:39:38 +0200

    authentication: add base app

---
+++ 3-authentication/src/test/java/wf/garnier/spring/security/base/BaseApplicationTests.java
@@ -0,0 +1,13 @@
+package wf.garnier.spring.security.base;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
