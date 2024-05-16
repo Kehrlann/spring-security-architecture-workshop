a0b3fab35054c63192289daa5ab41ab62234f3d6
Daniel Garnier-Moiroux <git@garnier.wf>
2024-05-16 17:06:14 +0200

    authorization: solution

--- 7-authorization/src/main/java/wf/garnier/spring/security/authorization/ConferenceService.java
+++ 7-authorization/src/main/java/wf/garnier/spring/security/authorization/ConferenceService.java
@@ -1,12 +1,15 @@
 package wf.garnier.spring.security.authorization;
 
-import org.springframework.stereotype.Component;
 import java.util.Collection;
 import java.util.List;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Component;
 
 @Component
 public class ConferenceService {
 
+    @PreAuthorize("@usernameAuthorizationService.isAuthorized(authentication.name)")
     public Collection<String> getConferences() {
         // These are all the conferences I have spoken at in 2023 :)
         return List.of(
