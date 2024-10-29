3c1dd9cd6f4f948522bc38faa4d18e2af6b15e61
Daniel Garnier-Moiroux <git@garnier.wf>
2024-10-29 14:06:39 +0100

    configurers: tests

--- 5-configurers/src/test/java/wf/garnier/spring/security/configurer/ConfigurerApplicationTests.java
+++ 5-configurers/src/test/java/wf/garnier/spring/security/configurer/ConfigurerApplicationTests.java
@@ -1,13 +1,50 @@
 package wf.garnier.spring.security.configurer;
 
 import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.web.servlet.assertj.MockMvcTester;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class ConfigurerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    MockMvcTester mvc;
+
+    @Test
+    void noHeader() {
+        mvc.get()
+                .uri("/private")
+                .exchange()
+                .assertThat()
+                .hasStatus(HttpStatus.UNAUTHORIZED)
+                .hasHeader("WWW-Authenticate", "Basic realm=\"Realm\"");
+    }
+
+    @Test
+    void withRobotHeader() {
+        mvc.get()
+                .uri("/private")
+                .header("x-robot-secret", "beep-boop")
+                .exchange()
+                .assertThat()
+                .hasStatus(HttpStatus.OK)
+                .bodyText()
+                .contains("Ms Robot");
+    }
+
+    @Test
+    void withRobotHeaderWrongSecret() {
+        mvc.get()
+                .uri("/private")
+                .header("x-robot-secret", "beep-beep")
+                .exchange()
+                .assertThat()
+                .hasStatus(HttpStatus.FORBIDDEN);
+    }
 
 }
