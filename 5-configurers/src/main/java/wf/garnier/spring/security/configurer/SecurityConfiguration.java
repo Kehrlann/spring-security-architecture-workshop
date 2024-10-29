8ad45db679a2e8fef7baaba485a4ad605156b0ab
Daniel Garnier-Moiroux <git@garnier.wf>
2024-10-29 14:02:17 +0100

    configurers: move SSO into a "docker" profile

--- 5-configurers/src/main/java/wf/garnier/spring/security/configurer/SecurityConfiguration.java
+++ 5-configurers/src/main/java/wf/garnier/spring/security/configurer/SecurityConfiguration.java
@@ -1,6 +1,9 @@
 package wf.garnier.spring.security.configurer;
 
+import java.util.Optional;
+
 import org.slf4j.LoggerFactory;
+
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,6 +13,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.intercept.AuthorizationFilter;
@@ -19,8 +23,11 @@ import org.springframework.security.web.access.intercept.AuthorizationFilter;
 public class SecurityConfiguration {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http
+    public SecurityFilterChain securityFilterChain(
+            HttpSecurity http,
+            Optional<ClientRegistrationRepository> clientRegistrationRepository
+    ) throws Exception {
+        var filterChainBuilder = http
                 .authorizeHttpRequests(authorize -> {
                     authorize.requestMatchers("/").permitAll();
                     authorize.requestMatchers("/css/**").permitAll();
@@ -31,13 +38,22 @@ public class SecurityConfiguration {
                 .formLogin(form -> {
                     form.defaultSuccessUrl("/private");
                 })
-                .oauth2Login(oidc -> {
-                    oidc.defaultSuccessUrl("/private");
-                })
                 .httpBasic(Customizer.withDefaults())
                 .addFilterBefore(new ForbiddenFilter(), AuthorizationFilter.class)
                 .addFilterBefore(new RobotAuthenticationFilter(), AuthorizationFilter.class)
-                .authenticationProvider(new DanielAuthenticationProvider())
+                .authenticationProvider(new DanielAuthenticationProvider());
+
+        // Conditionally enable OAuth2 login
+        // When spring.security.oauth2.client.registration... properties are set, such as in the "docker" profile,
+        // Spring Boot creates a ClientRegistrationRepository for you. In that case, we'll add OAuth2 login.
+        if (clientRegistrationRepository.isPresent()) {
+            filterChainBuilder
+                    .oauth2Login(oidc -> {
+                        oidc.defaultSuccessUrl("/private");
+                    });
+        }
+
+        return filterChainBuilder
                 .build();
     }
 
