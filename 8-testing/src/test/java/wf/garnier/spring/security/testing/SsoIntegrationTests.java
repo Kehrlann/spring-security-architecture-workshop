8bd75db6bf696403050bf64e7434c6b818f6ab79
Daniel Garnier-Moiroux <git@garnier.wf>
2024-11-08 14:31:52 +0100

    testing: add solutions

---
+++ 8-testing/src/test/java/wf/garnier/spring/security/testing/SsoIntegrationTests.java
@@ -0,0 +1,76 @@
+package wf.garnier.spring.security.testing;
+
+import java.io.IOException;
+
+import org.htmlunit.WebClient;
+import org.htmlunit.html.HtmlAnchor;
+import org.htmlunit.html.HtmlInput;
+import org.htmlunit.html.HtmlPage;
+import org.htmlunit.html.HtmlPasswordInput;
+import org.junit.jupiter.api.Test;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import wf.garnier.testcontainers.dexidp.DexContainer;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Testcontainers
+class SsoIntegrationTests {
+
+	private static final DexContainer.User ALICE = new DexContainer.User("alice", "alice@corp.example.com", "password");
+
+	private static final DexContainer.User BOB = new DexContainer.User("bob", "bob@example.com", "password");
+
+	@ServiceConnection
+	@Container
+	private static final DexContainer dexContainer = new DexContainer(DexContainer.DEFAULT_IMAGE_NAME).withUser(ALICE)
+		.withUser(BOB);
+
+	@LocalServerPort
+	private int port;
+
+	// Here we do not autowire a WebClient with @WebMvcTest, because that client
+	// can only talk to the Spring app, and wouldn't work with the Dex login page.
+	private final WebClient webClient = new WebClient();
+
+	@Test
+	void allowedDomain() throws IOException {
+		webClient.getOptions().setRedirectEnabled(true);
+
+		HtmlPage loginPage = webClient.getPage("http://localhost:%s/private".formatted(port));
+
+		HtmlPage dexLoginPage = loginPage.<HtmlAnchor>querySelector("table > tbody > tr > td > a").click();
+
+		dexLoginPage.<HtmlInput>getElementByName("login").type(ALICE.email());
+		dexLoginPage.<HtmlPasswordInput>getElementByName("password").type(ALICE.clearTextPassword());
+
+		HtmlPage appPage = dexLoginPage.getElementById("submit-login").click();
+
+		assertThat(appPage.getBody().getTextContent()).contains(ALICE.email());
+	}
+
+	@Test
+	void forbiddenDomain() throws IOException {
+		webClient.getOptions().setRedirectEnabled(true);
+
+		HtmlPage loginPage = webClient.getPage("http://localhost:%s/private".formatted(port));
+
+		HtmlPage dexLoginPage = loginPage.<HtmlAnchor>querySelector("table > tbody > tr > td > a").click();
+
+		dexLoginPage.<HtmlInput>getElementByName("login").type(BOB.email());
+		dexLoginPage.<HtmlPasswordInput>getElementByName("password").type(BOB.clearTextPassword());
+
+		HtmlPage appPage = dexLoginPage.getElementById("submit-login").click();
+
+		assertThat(appPage.getUrl()).hasPath("/login").hasParameter("error");
+
+		var errorPopup = appPage.querySelector("div[role=\"alert\"]");
+		assertThat(errorPopup.getTextContent()).contains(
+				"Cannot log in because email has domain [@example.com]. Only emails with domain [corp.example.com] are accepted.");
+	}
+
+}
--- 8-testing/src/test/java/wf/garnier/spring/security/testing/TestingApplicationTests.java
+++ 8-testing/src/test/java/wf/garnier/spring/security/testing/TestingApplicationTests.java
@@ -1,8 +1,183 @@
 package wf.garnier.spring.security.testing;
 
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
+import org.springframework.test.web.servlet.assertj.MockMvcTester;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.oidcLogin;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class TestingApplicationTests {
+
+	@Autowired
+	MockMvcTester mvc;
+
+	@Test
+	void publicPage() {
+		var result = mvc.get().uri("/");
+
+		//@formatter:off
+		assertThat(result)
+				.hasStatus(HttpStatus.OK)
+				.bodyText()
+				.contains("<h1>Hello world!</h1>");
+		//@formatter:on;
+	}
+
+	@Test
+	void forbiddenFilter() {
+		// You can also assert everything fluently by calling `.assertThat()` on the
+		// result of `.exchange()`
+		mvc.get()
+			.uri("/")
+			.header("x-forbidden", "true")
+			.exchange()
+			.assertThat()
+			.hasStatus(HttpStatus.FORBIDDEN)
+			.bodyText()
+			.isEqualTo("⛔⛔⛔⛔ this is forbidden");
+	}
+
+	@Test
+	void privatePageAnonymous() {
+		//@formatter:off
+		mvc.get()
+			.uri("/private")
+			.exchange()
+			.assertThat()
+			.hasStatus(HttpStatus.UNAUTHORIZED);
+		//@formatter:on
+	}
+
+	@Test
+	@WithMockUser("test-user")
+	void privatePageWithUser() {
+		mvc.get()
+			.uri("/private")
+			.exchange()
+			.assertThat()
+			.hasStatus(HttpStatus.OK)
+			.bodyText()
+			.contains("Hello, ~[test-user]~ !");
+	}
+
+	@Test
+	@WithMockUser(value = "test-user", roles = { "user" })
+	void adminPageNotAuthorized() {
+		mvc.get()
+			.uri("/admin")
+			.exchange()
+			.assertThat()
+			.hasStatus(HttpStatus.FOUND)
+			.redirectedUrl()
+			.isEqualTo("/private?denied");
+	}
+
+	@Test
+	@WithMockUser(value = "test-user", roles = { "user", "admin" })
+	void adminPageAuthorized() {
+		mvc.get()
+			.uri("/admin")
+			.exchange()
+			.assertThat()
+			.hasStatus(HttpStatus.OK)
+			.bodyText()
+			.contains("This is a very serious admin section. Only admins can see this page.");
+	}
+
+	@Test
+	void authenticationDaniel() {
+		mvc.get()
+			.uri("/private")
+			.with(SecurityMockMvcRequestPostProcessors.httpBasic("daniel", "lol-i-can-type-anything"))
+			.exchange()
+			.assertThat()
+			.hasStatus(HttpStatus.OK)
+			.bodyText()
+			.contains("Hello, ~[daniel]~ !");
+	}
+
+	@Test
+	void authenticationAlice() {
+		mvc.get()
+			.uri("/private")
+			.with(SecurityMockMvcRequestPostProcessors.httpBasic("alice", "password"))
+			.exchange()
+			.assertThat()
+			.hasStatus(HttpStatus.OK)
+			.bodyText()
+			.contains("Hello, ~[alice]~ !");
+	}
+
+	@Test
+	void authenticationAliceWrongPassword() {
+		mvc.get()
+			.uri("/private")
+			.with(SecurityMockMvcRequestPostProcessors.httpBasic("alice", "wrong-password"))
+			.exchange()
+			.assertThat()
+			.hasStatus(HttpStatus.UNAUTHORIZED);
+	}
+
+	@Test
+	void formLoginDaniel() {
+		mvc.post()
+			.uri("/login")
+			.param("username", "daniel")
+			.param("password", "foobar")
+			.with(SecurityMockMvcRequestPostProcessors.csrf())
+			.exchange()
+			.assertThat()
+			.hasStatus(HttpStatus.FOUND)
+			.redirectedUrl()
+			.isEqualTo("/private");
+	}
+
+	@Test
+	void formLoginAlice() {
+		mvc.post()
+			.uri("/login")
+			.param("username", "alice")
+			.param("password", "password")
+			.with(SecurityMockMvcRequestPostProcessors.csrf())
+			.exchange()
+			.assertThat()
+			.hasStatus(HttpStatus.FOUND)
+			.redirectedUrl()
+			.isEqualTo("/private");
+	}
+
+	@Test
+	void formLoginAliceWrongPassword() {
+		mvc.post()
+			.uri("/login")
+			.param("username", "alice")
+			.param("password", "wrong-password")
+			.with(SecurityMockMvcRequestPostProcessors.csrf())
+			.exchange()
+			.assertThat()
+			.hasStatus(HttpStatus.FOUND)
+			.redirectedUrl()
+			.isEqualTo("/login?error");
+	}
+
+	@Test
+	void ssoLogin() {
+		mvc.get()
+			.uri("/private")
+			.with(oidcLogin().idToken(token -> token.claim("email", "test@example.com")))
+			.exchange()
+			.assertThat()
+			.hasStatus(HttpStatus.OK)
+			.bodyText()
+			.contains("Hello, ~[test@example.com]~ !");
+	}
 
 }
