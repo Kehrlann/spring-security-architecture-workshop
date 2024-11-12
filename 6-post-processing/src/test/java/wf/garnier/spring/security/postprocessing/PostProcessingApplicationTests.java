f675f9ef49c0321c4154b948ee1d9f6bbefbf074
Daniel Garnier-Moiroux <git@garnier.wf>
2024-11-12 15:31:04 +0100

    post-processing: remove broken test

--- 6-post-processing/src/test/java/wf/garnier/spring/security/postprocessing/PostProcessingApplicationTests.java
+++ 6-post-processing/src/test/java/wf/garnier/spring/security/postprocessing/PostProcessingApplicationTests.java
@@ -2,12 +2,10 @@ package wf.garnier.spring.security.postprocessing;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 class PostProcessingApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
 
 }
