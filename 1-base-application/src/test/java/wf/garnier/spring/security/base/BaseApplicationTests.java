eb4167ec8d732d50bd599538b865585be0d41a15
Daniel Garnier-Moiroux <git@garnier.wf>
2024-11-12 15:37:40 +0100

    base-application: remove broken test

--- 1-base-application/src/test/java/wf/garnier/spring/security/base/BaseApplicationTests.java
+++ 1-base-application/src/test/java/wf/garnier/spring/security/base/BaseApplicationTests.java
@@ -1,13 +1,8 @@
 package wf.garnier.spring.security.base;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BaseApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
